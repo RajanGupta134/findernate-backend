951c1da40caa7cba4a6fd53e06bb5ab0fe57806a
Amit <amitantu7277@gmail.com>
2025-09-25 16:03:28 +0530

    changes done

--- src/app.js
+++ src/app.js
@@ -28,7 +28,8 @@ app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 const allowedOrigins = [
         "https://eckss0cw0ggco0okoocc4wo4.194.164.151.15.sslip.io",
         "https://p0k804os4c4scowcg488800c.194.164.151.15.sslip.io",
-        "http://localhost:3000"
+        "http://localhost:3000",
+        "http://localhost:4000"
 ];
 
 
