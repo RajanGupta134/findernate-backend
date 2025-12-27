8af431d22210c36fc09fdaaa1fe2c915d3a6179f
Boomlex <127718853+BoomlexTech@users.noreply.github.com>
2025-12-27 10:32:57 +0530

    dones

--- src/config/socket.js
+++ src/config/socket.js
@@ -29,7 +29,7 @@ class SocketManager {
                 "http://127.0.0.1:4000",
                 "https://z0n8vrlt-4000.inc1.devtunnels.ms",
                 /^https?:\/\/[\w-]+\.194\.164\.151\.15\.sslip\.io$/,
-                "https://findernate.com"
+                "https://apis.findernate.com"
                 // Allow all local network IPs for development
                 ...(process.env.NODE_ENV === 'development' ? [/^http:\/\/192\.168\.\d+\.\d+:4000$/] : []),
             ];
